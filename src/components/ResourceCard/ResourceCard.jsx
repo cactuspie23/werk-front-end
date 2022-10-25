cc4456a393b74cd1eea120c1957bb4e47ec15f86
laurenhatchett <laurenhatchett@gmail.com>
2022-10-25 14:53:48 -0400

    update ResourceList, AddResource, and ResourceCard

--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -2,11 +2,13 @@
 const ResourceCard = ({ resource }) => {
   return (
     <>
+    <div>
     <h1>{resource.name}</h1>
     <p>{resource.URL}</p>
     <p>{resource.description}</p>
     <p>{resource.category}</p>
     <p>{resource.skills}</p>
+    </div>
     </>
   )
 }
