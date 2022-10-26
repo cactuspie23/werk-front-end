cbce47cc36001fe9752f5b94550ed346e25e07b9
AlyssaD <pink_lime@live.com>
2022-10-25 21:57:23 -0400

    set up profile  logs

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ const App = () => {
           path="/profiles"
           element={
             <ProtectedRoute user={user}>
-              <Profiles />
+              <Profiles user={user} />
             </ProtectedRoute>
           }
         />
