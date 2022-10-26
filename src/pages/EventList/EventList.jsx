8f2238bfd7025f49f9096e3fe6e956934f5319e9
racssett <rachel.c.hassett@gmail.com>
2022-10-26 13:11:33 -0500

    Push delete and update functionality

--- src/pages/EventList/EventList.jsx
+++ src/pages/EventList/EventList.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
 import EventCard from "../../components/EventCard/EventCard"
 import * as eventService from '../../services/eventService'
 
