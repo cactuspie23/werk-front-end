410fe42fd1f725e86374579980d395c990ea017e
laurenhatchett <laurenhatchett@gmail.com>
2022-10-26 10:31:15 -0400

    implement update and delete functionality on ResourceDetails

--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 
 const ResourceCard = ({ resource }) => {
   return (
     <>
     <div>
-    <h1>{resource.name}</h1>
+    <Link to={`/resources/${resource._id}`}>
+      <h1>{resource.name}</h1>
+    </Link>
     <p>{resource.URL}</p>
     <p>{resource.description}</p>
     <p>{resource.category}</p>
---
+++ src/pages/ResourceDetails/ResourceDetails.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import * as resourceService from '../../services/resourceService'
+
+
+const ResourceDetails = (props) => {
+  const { id } = useParams()
+  const [resource, setResource] = useState(null)
+
+  useEffect(() => {
+    const fetchResource = async () => {
+      const data = await resourceService.show(id)
+      setResource(data)
+    }
+    fetchResource()
+  }, [id])
+
+  if (!resource) return <main> Loading...</main>
+
+  return (
+    <main>
+      <article>
+          <h1>{resource.name}</h1>
+          <h3><Link>{resource.URL}</Link></h3>
+          <h3>{resource.description}</h3>
+          <h3>{resource.category}</h3>
+          <h3>{resource.skills}</h3>
+          </article>
+        <span>
+            {resource.owner._id === props.user.profile &&
+              <>
+                <Link to={`/resources/${id}/edit`} state={resource}>Edit</Link>
+                <button onClick={() => props.handleDeleteResource(id)}
+                >
+                  Delete
+                </button>
+              </>
+            }
+        </span>
+    </main>
+  )
+}
+
+export default ResourceDetails
