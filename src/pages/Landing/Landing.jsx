73c7c90dad215dcbf9d66e8f8cb34016da6e84d1
Leon Chu <Ljchu87@gmail.com>
2022-10-27 08:53:13 -0500

    Add css to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,20 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      {/* <h1>hello, {user ? user.name : 'friend'}</h1> */}
+      <section className={styles.splash}>
+        <img src="/images/werk-logo.png" alt="WERK logo" />
+      </section>
+      <section className={styles.about}>
+        <header>
+          <h3>ABOUT WERK.</h3>
+        </header>
+        <article>
+          <p>
+            Werk was founded to empower job seekers with the necessary tools they need to stand out and get hired. Track down relevant opportunities tailored to your professional needs and start building your better future. Werk smarter, not harder.
+          </p>
+        </article>
+      </section>
     </main>
   )
 }
