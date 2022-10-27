20f0acd3c28c42e38e26e3fdbb40bf858f6c5707
Leon Chu <Ljchu87@gmail.com>
2022-10-27 13:33:45 -0500

    Change text align on p tag

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -12,8 +12,8 @@ const Landing = ({ user }) => {
           <h3>ABOUT WERK.</h3>
         </header>
         <article>
-          <p>
-            Werk was founded to empower job seekers with the necessary tools they need to stand out and get hired. Track down relevant opportunities and networking events tailored to your professional needs and start building your better future. Whether you’re a seasoned expert or just starting your career in the world of tech, it’s important that you’re always learning and keeping pace with this growing industry. There are thousands of resources that allow you to acquire the skills you need to land your next job and Werk allows you to keep them all organized so you don’t have to. Whether you are looking for a tool to record your daily coding accomplishments or keep track of your job search process, we have you covered. 
+          <p className={styles.aboutP}>
+            Werk was founded to empower job seekers with the necessary tools they need to stand out and get hired. Track down relevant opportunities and networking events tailored to your professional needs and start building your better future. Whether you’re a seasoned expert or just starting your career in the world of tech, it’s important that you’re always learning and keeping pace with this growing industry. There are thousands of resources that allow you to acquire the skills you need to land your next job and Werk allows you to keep them all organized so you don’t have to. Whether you are looking for a tool to record your daily coding accomplishments or keep track of your job search process, we have you covered. Werk gives you all the tools you need to build a better future. Werk smarter, not harder. 
           </p>
         </article>
       </section>
